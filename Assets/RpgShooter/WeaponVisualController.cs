57cbf001606e5e64beeb746bc337b0a8a99da593
gaoxiang1 <@ztgame.com>
2025-03-27 22:46:23 +0800

    33 lefthandIk

--- Assets/RpgShooter/WeaponVisualController.cs
+++ Assets/RpgShooter/WeaponVisualController.cs
@@ -1,10 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
+
+public enum Weapon_Grab_Type
+{
+    SideGrab,
+    BackGrab,
+}
 
 public class WeaponVisualController : MonoBehaviour
 {
+    private Animator anim;
     [SerializeField] private Transform[] gunTransforms;
     [SerializeField] private Transform pistol;
     [SerializeField] private Transform revolver;
@@ -14,33 +19,86 @@ public class WeaponVisualController : MonoBehaviour
 
     private Transform currentGun;
     [Header("left hand IK")]
-    [SerializeField] private Transform leftHand;
+    [SerializeField] private TwoBoneIKConstraint leftHandIK;
+    [SerializeField] private Transform leftHandTarget;
+
+    [Header("rig")]
+    private Rig rig;
+    private bool rigShouldBeIncreased;
+    [SerializeField] private float rigIncreaseSpeed;
 
     private void Start()
     {
         SwitchOn(pistol);
+        anim = GetComponentInChildren<Animator>();
+        rig = GetComponentInChildren<Rig>();
     }
 
-    private void Update() {
-        if(Input.GetKeyDown(KeyCode.Alpha1))
+    private void Update()
+    {
+        CheckWeaponSwitch();
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            anim.SetTrigger("Reload");
+            PauseRig();
+        }
+
+        if (rigShouldBeIncreased)
+        {
+            rig.weight += rigIncreaseSpeed * Time.deltaTime;
+            if(rig.weight >= 1)
+            {
+                rigShouldBeIncreased = false;
+            }
+        }
+    }
+
+    private void PauseRig()
+    {
+        rig.weight = 0.15f;
+    }
+
+    public void ReturnRigWeightToOne() => rigShouldBeIncreased = true;
+    private void PlayWeaponGrabAnimation(Weapon_Grab_Type grabType)
+    {
+        PauseRig();
+        leftHandIK.weight = 0;
+        anim.SetBool("BusyGrabbingWeapon", true);
+        anim.SetFloat("WeaponGrabType", (float)grabType);
+        anim.SetTrigger("WeaponGrab");
+    }
+    
+    private void CheckWeaponSwitch()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             SwitchOn(pistol);
+            SwitchAnimationLayer(1);
+            PlayWeaponGrabAnimation(Weapon_Grab_Type.SideGrab);
         }
-        if(Input.GetKeyDown(KeyCode.Alpha2))
+        if (Input.GetKeyDown(KeyCode.Alpha2))
         {
             SwitchOn(revolver);
+            SwitchAnimationLayer(1);
+            PlayWeaponGrabAnimation(Weapon_Grab_Type.SideGrab);
         }
-        if(Input.GetKeyDown(KeyCode.Alpha3))
+        if (Input.GetKeyDown(KeyCode.Alpha3))
         {
             SwitchOn(autoRifle);
+            SwitchAnimationLayer(1);
+            PlayWeaponGrabAnimation(Weapon_Grab_Type.SideGrab);
         }
-        if(Input.GetKeyDown(KeyCode.Alpha4))
+        if (Input.GetKeyDown(KeyCode.Alpha4))
         {
             SwitchOn(shotGun);
+            SwitchAnimationLayer(2);
+            PlayWeaponGrabAnimation(Weapon_Grab_Type.BackGrab);
         }
-        if(Input.GetKeyDown(KeyCode.Alpha5))
+        if (Input.GetKeyDown(KeyCode.Alpha5))
         {
             SwitchOn(sniperRifle);
+            SwitchAnimationLayer(3);
+            PlayWeaponGrabAnimation(Weapon_Grab_Type.BackGrab);
         }
     }
 
@@ -63,7 +121,22 @@ public class WeaponVisualController : MonoBehaviour
     private void AttachLeftHand()
     {
         Transform targetTransform = currentGun.GetComponentInChildren<LeftHandTransform>().transform;
-        leftHand.localPosition = targetTransform.localPosition;
-        leftHand.localRotation = targetTransform.localRotation;
+        leftHandTarget.localPosition = targetTransform.localPosition;
+        leftHandTarget.localRotation = targetTransform.localRotation;
+    }
+
+    private void SwitchAnimationLayer(int layerIndex)
+    {
+        for (int i = 1; i < anim.layerCount; i++)
+        {
+            if(i == layerIndex)
+            {
+                anim.SetLayerWeight(i, 1);
+            }
+            else
+            {
+                anim.SetLayerWeight(i, 0);
+            }
+        }
     }
 }
